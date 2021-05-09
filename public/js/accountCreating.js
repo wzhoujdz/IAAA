ef9f99dc19477f72dad0025413f00b4d8124b5e1
wz <ruby_18@163.com>
2021-05-09 20:58:06 +0800

    前后端通

--- public/js/accountCreating.js
+++ public/js/accountCreating.js
@@ -1,90 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<style type="text/css">
-	</style>
-    <script type="text/javascript">
-		
-	</script> 
-</head>
-<body style="margin-left:0px;">
-    <div style="width: 1024px;height:1024px;font-weight:400;background:white;margin:0 auto">
-	<!-- 最大的狂 -->
-		<div style = "float:left;width:1024px;background:white;">
-				<div style = "text-align:center;font-size:25px;color:dodgerblue;font-weight:600;">
-					Creat account
-				</div>
-				<div style = "float:left;margin-left:290px;margin-top:45px;font-size:15px;color:gray;font-weight: 450;">
-					Username
-				</div>
-				<div style="clear:both"></div>
-				<!-- 文本框 -->
-				
-				<div style="clear:both"></div>
-				
-				<div style = "float:left;margin-left:290px;margin-top:50px;font-size:15px;color:gray;font-weight: 450;">
-					Password
-                </div>
-                <div style="clear:both"></div>
-				
-                <div style = "float:left;margin-left:290px;margin-top:50px;font-size:15px;color:gray;font-weight: 450;">
-					Confirm Password
-                </div>
-                <div style="clear:both"></div>
-                
-		</div>
-		
-		<div style = "float:left;margin-left:600px;width:1024px;background:white;margin-top:40px;color:black;font-size:35px;">
-			<button onClick="alertTips()">
-                create
-            </button>
-            <script>
-                function alertTips(){
-                    alert("Your account has been created successfully");
-                    self.location='Sign in.html';
-                }
-            </script>
-			<div style="clear:both"></div>
-		</div>
-	</div>
-</body>
+let submitButton = document.getElementById("submitButton")
+submitButton.addEventListener("click", function(){
+    var userName=document.getElementById('userName').value;
+    var phoneOrEmail=document.getElementById('phoneOrEmail').value;
+    var password=document.getElementById('password').value;
+    //获取这个id的所在的节点
+    var confirmPassword=document.getElementById('confirmPassword').value;
+    var securityAnswer=document.getElementById('securityAnswer').value;
+    if(password==confirmPassword){
+        var data = {
+            userName: userName,
+            phoneOrEmail:phoneOrEmail,
+            password: password,
+            securityAnswer:securityAnswer
+        }
+        fetch('/signIn', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data), // post body
+          })
+          .then(response => response.json())
+          .then(data => {
+            console.log('Past Activity Success:', data);
+          })
+    }else{
+        alert('password is not the same as confirm password');
+    }
 
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
