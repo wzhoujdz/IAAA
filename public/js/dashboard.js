5ece6900b5c092ce5ad4540f22cd0dc0e0210951
wz <ruby_18@163.com>
2021-05-16 21:31:05 +0800

    20210516

---
+++ public/js/dashboard.js
@@ -0,0 +1,36 @@
+let calculationId = document.getElementById("calculationId")
+
+calculationId.addEventListener("click", function(){
+  window.location.href="../signIn.html"
+});
+
+
+let calculationImgId = document.getElementById("calculationImgId")
+
+calculationImgId.addEventListener("click",function(){
+  window.location.href="../signIn.html"
+})
+
+let chartId = document.getElementById("chartId")
+
+chartId.addEventListener("click", function(){
+  window.location.href="../signIn.html"
+});
+
+let chartImgId = document.getElementById("chartImgId")
+
+chartImgId.addEventListener("click",function(){
+  window.location.href="../signIn.html"
+})
+
+let goodSearchImgId = document.getElementById("goodSearchImgId")
+
+goodSearchImgId.addEventListener("click",function(){
+  window.location.href="../signIn.html"
+})
+
+let goodSearchId = document.getElementById("goodSearchId")
+
+goodSearchId.addEventListener("click",function(){
+  window.location.href="../signIn.html"
+})
