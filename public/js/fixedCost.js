39adcde2cb42fb8bbfe86f57a1347142508189dc
wz <ruby_18@163.com>
2021-07-25 15:08:45 +0800

    add back to calculation

--- public/js/fixedCost.js
+++ public/js/fixedCost.js
@@ -2,3 +2,7 @@ let homeID = document.getElementById("homeID");
 homeID.addEventListener("click", function(){
     window.location.href="../dashboard.html"
 });
+let calculationId = document.getElementById("calculationId");
+calculationId.addEventListener("click", function(){
+    window.location.href="../calculation.html"
+});
---
+++ public/js/profits.js
@@ -0,0 +1,8 @@
+let homeID = document.getElementById("homeID");
+homeID.addEventListener("click", function(){
+    window.location.href="../dashboard.html"
+});
+let calculationId = document.getElementById("calculationId");
+calculationId.addEventListener("click", function(){
+    window.location.href="../calculation.html"
+});
