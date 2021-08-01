e17e03a089e353772b9f7ff7abcbdb024da01ac0
wz <ruby_18@163.com>
2021-08-01 15:27:28 +0800

    81

--- public/js/variableCost.js
+++ public/js/variableCost.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
         window.location.href="../calculation.html"
     });
     
+    let addId=document.getElementById("addId");
+    addId.addEventListener("click", function(){
+        $("#space").append(
+        '<div style = "float:left;margin-left: 40px; font-size:20px;margin-top:5px">'+
+            '<select id="materialName" class="input">'+
+                '<option value="duckNeck">duck neck</option>'+
+                '<option value="tofu">tofu</option>'+
+            '</select>'+
+        '</div>'+
+        '<div style = "float:left;margin-left: 35px; font-size:20px;margin-top:5px;">'+               
+            '<input style= "width: 120px;height:25px" type="text" id="materialNumber" />'+
+        '</div>'+
+        '<div style = "float:left;margin-left: 35px; font-size:20px;margin-top:5px;">'+                    
+            '<input style= "width: 120px;height:25px" type="text" id="materialCost" />'+
+        '</div> '
+        )
+    });
 });
