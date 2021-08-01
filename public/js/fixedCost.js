2780fd13739bc18b1d9e8487bdddbcb06a83e4a0
wz <ruby_18@163.com>
2021-08-01 14:29:20 +0800

    fixed change

--- public/js/fixedCost.js
+++ public/js/fixedCost.js
@@ -10,7 +10,30 @@ $(document).ready(function(){
     
     let submitId = document.getElementById("submitId");
     submitId.addEventListener("click", function(){
+        var employmentFees=$("#employmentFees").val();
+        console.log(employmentFees);
         
+        var rentalFees = $("#rentalFees").val();
+        console.log(rentalFees);
+
+        var utilityFees = $("#utilityFees").val();
+        console.log(utilityFees);
+
+        var total = parseFloat(employmentFees)+parseFloat(rentalFees)+parseFloat(utilityFees);
+        console.log(total);
+
+
+        $("#space").append(
+            '<div class="format">'+
+                'employment fees: $'+employmentFees+
+            '</div>'+
+            '<div class="format">'+
+                'rental fees: $' + rentalFees+
+            '</div>'+
+            '<div class="format">'+
+                'utility fees: $' + utilityFees+
+            '</div>'
+        )
     });
     
 });
