c106d5f800861ad5799cc559f5e851daefe68922
wz <ruby_18@163.com>
2021-08-08 14:57:54 +0800

    fooddd

--- public/js/revenue.js
+++ public/js/revenue.js
@@ -7,5 +7,30 @@ $(document).ready(function(){
     calculationId.addEventListener("click", function(){
         window.location.href="../calculation.html"
     });
+    let spaceItemNumber=1;
+    let addId=document.getElementById("addId");
+    addId.addEventListener("click", function(){
+        spaceItemNumber=spaceItemNumber+1;
+        $("#space").append(
+        '<div style = "float:left;margin-left: 35px; font-size:20px;margin-top:5px">'+
+            '<select id="foodName'+spaceItemNumber+'" class="input">'+
+                '<option value="spicy duck neck">spicy duck neck</option>'+
+                '<option value="cured meat">cured meat</option>'+
+                ' <option value="marinated trotters">marinated trotters</option>'+ 
+            '</select>'+
+        '</div>'+
+        '<div style = "float:left;margin-left: 30px; font-size:20px;margin-top:5px;">'+                    
+                '<input style= "width: 90px;height:25px" type="text" id="foodNumber'+spaceItemNumber+'" />'+
+        '</div>'+
+        '<div style = "float:left;margin-left: 30px; font-size:20px;margin-top:5px;">'+                  
+                '<input style= "width: 90px;height:25px" type="text" id="foodPrice'+spaceItemNumber+'" />'+
+            '</div> '+
+        '<div style = "float:left;margin-left:30px; font-size:20px;margin-top:5px;">  '   +                
+            '<input style= "width: 90px;height:25px" type="text" id="discount'+spaceItemNumber+'" />'+
+        '</div> '
+        )
+    });
+
+    
     
 });
